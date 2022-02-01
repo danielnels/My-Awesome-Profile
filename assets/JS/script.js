de4d956ace728163741a67e88fd0194b71c8d979
danielnels <danieln@newground.net.au>
2022-02-01 18:06:52 +0800

    Added images and initail code

---
+++ assets/JS/script.js
@@ -0,0 +1,33 @@
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+
+function sendMail() {
+    var link = "mailto:danieln@newground.net.au"
+      + "?cc=danieln@newground.net.au"
+      + "&subject=" + encodeURIComponent("request info")
+      + "&body=" + encodeURI(document.location);
+    
+  
+    alert("Your e-mail is ready to be sent!");
+  
+    window.location.href = link;
+    
+     
+}
