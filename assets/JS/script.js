a44d62f55feda6e211e5304a4cf862a9ab6954c8
danielnels <danieln@newground.net.au>
2022-02-05 10:01:38 +0800

    Made contact me form fully functional

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -17,6 +17,18 @@ navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
 
+var currentDay = moment().format('MMMM Do YYYY');
+document.querySelector("#currentDay").innerHTML = (`${currentDay}`);
+console.log(currentDay);
+
+var currentHr = moment().format('HH');
+
+var currentDay = moment().format('MMMM Do YYYY');
+document.querySelector("#currentDay").innerHTML = (`${currentDay}`);
+console.log(currentDay);
+
+var currentHr = moment().format('HH');
+console.log(currentHr);
 function showSkills() {
   let node = document.getElementById('hide')
   node.style.visibility = 'visible';
@@ -226,21 +238,55 @@ function showSk() {
 }
 
   
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
 
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
 
+  // Success and Error functions for after the form is submitted
 
-  
-  
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks Your E-mail has been Sent!";
+  }
 
-function sendMail() {
-    let link = "mailto:danieln@newground.net.au"
-      + "?cc=danieln@newground.net.au"
-      + "&subject=" + encodeURIComponent("request info")
-      + "&body=" + encodeURI(document.location);
-    
-  
-    alert("Your e-mail is ready to be sent!");
-  
-    window.location.href = link;
-        
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
 }
+
+
+  
+  
+
+        
+
